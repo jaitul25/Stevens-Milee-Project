44fee23291784266ba23c000b4d34c941a3043c6
RuchaCB <RuchaCB@users.noreply.github.com>
2020-10-31 02:57:21 -0400

    Angular UI Second attempt

---
+++ StevensMileeUI/src/app/course.ts
@@ -0,0 +1,10 @@
+export class Course {
+    courseId: number;
+    name: string;
+    description: string;
+    semester: string;
+    rating: number;
+    teachingMode: string;
+    majorId: number;
+    ProfId: number;
+}
